55e3c14eb3f70bca242fba30f5ae55ee42bf7e88
DilshanHarendra <harendracripto@gmail.com>
2021-08-23 23:22:19 +0530

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const htmlWebpackPlugin= require('html-webpack-plugin')
+module.exports={
+    entry: './src/index.js',
+    output: {
+        filename:'main.js',
+        path: path.resolve(__dirname,'/dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude:/node_modules/,
+                use: {
+                    loader:'babel-loader',
+                }
+            }
+
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+}
